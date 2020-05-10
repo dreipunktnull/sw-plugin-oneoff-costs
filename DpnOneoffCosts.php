2754ca35761590b7c6b6fb935fff23afb1113cc4
Björn Fromme <fromme@dreipunktnull.com>
2020-05-10 16:04:26 +0200

    Revoke custom attribute form extension

--- DpnOneoffCosts.php
+++ DpnOneoffCosts.php
@@ -10,10 +10,8 @@ namespace DpnOneoffCosts;
  * All rights reserved
  */
 
-use DpnOneoffCosts\Service\Installer;
-use DpnOneoffCosts\Service\Uninstaller;
-use DpnOneoffCosts\Service\Updater;
 use Shopware\Bundle\AttributeBundle\Service\CrudServiceInterface;
+use Shopware\Bundle\AttributeBundle\Service\TypeMapping;
 use Shopware\Components\Model\ModelManager;
 use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\ActivateContext;
@@ -29,22 +27,29 @@ class DpnOneoffCosts extends Plugin
      */
     public function install(InstallContext $context)
     {
-        /** @var CrudServiceInterface $crudService */
-        $crudService = $this->container->get('shopware_attribute.crud_service');
+        try {
+            $this->createOrUpdateAttributes();
+        }
+        catch (\Exception $e) {
+            $context->scheduleMessage($e->getMessage());
+        }
         /** @var ModelManager $modelManager */
         $modelManager = $this->container->get('models');
-        $installer = new Installer($crudService, $modelManager, $context);
-        $installer->install();
+        $modelManager->generateAttributeModels(['s_articles_attributes']);
     }
 
     public function update(UpdateContext $context)
     {
-        /** @var CrudServiceInterface $crudService */
-        $crudService = $this->container->get('shopware_attribute.crud_service');
+        try {
+            $this->createOrUpdateAttributes();
+        }
+        catch (\Exception $e) {
+            $context->scheduleMessage($e->getMessage());
+        }
         /** @var ModelManager $modelManager */
         $modelManager = $this->container->get('models');
-        $updater = new Updater($crudService, $modelManager, $context);
-        $updater->update();
+        $modelManager->generateAttributeModels(['s_articles_attributes']);
+        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
     }
 
     /**
@@ -52,7 +57,7 @@ class DpnOneoffCosts extends Plugin
      */
     public function activate(ActivateContext $context)
     {
-        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
+        $context->scheduleClearCache(ActivateContext::CACHE_LIST_DEFAULT);
     }
 
     /**
@@ -61,15 +66,9 @@ class DpnOneoffCosts extends Plugin
     public function uninstall(UninstallContext $context)
     {
         if (!$context->keepUserData()) {
-            /** @var CrudServiceInterface $crudService */
-            $crudService = $this->container->get('shopware_attribute.crud_service');
-            /** @var ModelManager $modelManager */
-            $modelManager = $this->container->get('models');
-            $uninstaller = new Uninstaller($crudService, $modelManager, $context);
-            $uninstaller->uninstall();
+            $this->removeAttributes($context);
         }
-
-        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
+        $context->scheduleClearCache(UninstallContext::CACHE_LIST_DEFAULT);
     }
 
     /**
@@ -78,6 +77,116 @@ class DpnOneoffCosts extends Plugin
     public function deactivate(DeactivateContext $context)
     {
         $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
+    }
+
+    protected function createOrUpdateAttributes()
+    {
+        /** @var CrudServiceInterface $crudService */
+        $crudService = $this->container->get('shopware_attribute.crud_service');
+
+        $crudService->update(
+            's_articles_attributes',
+            'oneoff_costs_price',
+            TypeMapping::TYPE_FLOAT,
+            [
+                'displayInBackend' => true,
+                'position' => 300,
+                'custom' => false,
+                'translatable' => false,
+                'label' => 'Price',
+                'helpText' => 'One-off costs to be added to this article independent from amount',
+            ]
+        );
+
+        $crudService->update(
+            's_articles_attributes',
+            'oneoff_costs_price_net',
+            TypeMapping::TYPE_BOOLEAN,
+            [
+                'displayInBackend' => true,
+                'position' => 301,
+                'custom' => false,
+                'translatable' => false,
+                'label' => 'Price net',
+                'helpText' => 'One-off costs price is entered net',
+            ]
+        );
+
+        $crudService->update(
+            's_articles_attributes',
+            'oneoff_costs_tax',
+            TypeMapping::TYPE_SINGLE_SELECTION,
+            [
+                'displayInBackend' => true,
+                'allowBlank' => true,
+                'position' => 302,
+                'custom' => false,
+                'translatable' => false,
+                'entity' => 'Shopware\Models\Tax\Tax',
+                'label' => 'Tax',
+                'helpText' => 'Tax to be applied to one-off costs (article\'s tax rule is used by default)',
+            ]
+        );
+
+        $crudService->update(
+            's_articles_attributes',
+            'oneoff_costs_label',
+            TypeMapping::TYPE_STRING,
+            [
+                'displayInBackend' => true,
+                'position' => 303,
+                'custom' => false,
+                'translatable' => true,
+                'label' => 'Label',
+                'helpText' => 'One-off costs label shown in basket',
+            ]
+        );
+
+        $crudService->update(
+            's_articles_attributes',
+            'oneoff_costs_ordernum',
+            TypeMapping::TYPE_STRING,
+            [
+                'displayInBackend' => true,
+                'position' => 304,
+                'custom' => false,
+                'translatable' => false,
+                'label' => 'Order number',
+                'helpText' => 'One-off costs order number',
+            ]
+        );
+    }
+
+    /**
+     * @param UninstallContext $context
+     */
+    protected function removeAttributes(UninstallContext $context)
+    {
+        /** @var CrudServiceInterface $crudService */
+        $crudService = $this->container->get('shopware_attribute.crud_service');
+
+        $attributes = [
+            'oneoff_costs_price',
+            'oneoff_costs_label',
+            'oneoff_costs_tax',
+            'oneoff_costs_ordernum'
+        ];
+
+        if ($context->assertMinimumVersion('1.2')) {
+            $attributes[] = 'oneoff_costs_price_net';
+        }
+
+        try {
+            foreach ($attributes as $attribute) {
+                $crudService->delete('s_articles_attributes', $attribute);
+            }
+        }
+        catch (\Exception $e) {
+            $context->scheduleMessage($e->getMessage());
+        }
+        /** @var ModelManager $modelManager */
+        $modelManager = $this->container->get('models');
+        $modelManager->generateAttributeModels(['s_articles_attributes']);
     }
 }
 
--- Resources/views/backend/dpn_oneoff_costs/article/app.js
+++
@@ -1,4 +0,0 @@
-//{block name="backend/article/application"}
-//  {$smarty.block.parent}
-//  {include file="backend/dpn_oneoff_costs/article/view/detail/oneoffcosts.js"}
-//{/block}
